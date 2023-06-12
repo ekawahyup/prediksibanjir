bb587d47ed4a4d54902d7f1006c6fdaa64af31d3
ekawahyup <41234224+ekawahyup@users.noreply.github.com>
2023-06-12 20:57:24 +0700

    Update menubanjir.py

--- menubanjir.py
+++ menubanjir.py
@@ -1,4 +1,9 @@
 import streamlit as st
+
+st.title("Aplikasi Prediksi Banjir")
+st.hidder("Kelompok 8 Tugas Datascience PJJ AMIKOM")
+
+
 st.write("""
 #Aplikasi banjir
 Ini adalah prediksi banjir""")
